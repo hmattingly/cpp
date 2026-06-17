fc4e719d8674da1ac5d0d3a7ce8531ef3e9cfa4a
hmattingly <hmattingly8@gmail.com>
2026-06-16 17:40:16 -0700

    Add files via upload

---
+++ training/include/functionPointers.h
@@ -0,0 +1,8 @@
+// ------- Header Guard -------
+#ifndef FUNCPOINT_H  // variable typically matches header file name in all caps
+#define FUNCPOINT_H  // effectively creating a macro that replaces variable with nothing
+ 
+// ------- Forward Declarations -------
+void functionPractice();
+
+#endif
