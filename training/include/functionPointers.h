b01dee5c54ba992460b46a5d99d31cf030680a30
hmattingly <hmattingly8@gmail.com>
2026-06-22 09:25:19 -0700

    Delete training/include/functionPointers.h

--- training/include/functionPointers.h
+++
@@ -1,8 +0,0 @@
-// ------- Header Guard -------
-#ifndef FUNCPOINT_H  // variable typically matches header file name in all caps
-#define FUNCPOINT_H  // effectively creating a macro that replaces variable with nothing
- 
-// ------- Forward Declarations -------
-void functionPractice();
-
-#endif
